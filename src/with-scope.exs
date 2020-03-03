b2b831c6454a8ba76464b04b46242833b1775a31
Renato <renato.scaroni@caminoeducation.com>
2020-03-03 13:52:21 -0300

    benning studies

---
+++ src/with-scope.exs
@@ -0,0 +1,17 @@
+content = "whatever"
+
+lp = with {:ok, file} = File.open("/etc/passwd"),
+        content = IO.read(file, :all),
+        :ok = File.close(file),
+        [_, uid, gid] = Regex.run(~r/^lp:.*?:(\d+):(\d+)/m, content)
+    do
+        "Group: #{gid}, User: #{uid}"
+    end
+
+IO.puts lp #=> Group: 26, User: 26
+IO.puts content #=> Now is the time
+
+
+if content == "whatever" do
+    IO.puts "bla"
+end
