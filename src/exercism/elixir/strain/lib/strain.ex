ded8e8b6bb0cd76522ade622b8dcd505807a6a6b
Renato <renato.scaroni@caminoeducation.com>
2020-03-10 15:20:20 -0300

    Adding hello world, strain and space age problems

---
+++ src/exercism/elixir/strain/lib/strain.ex
@@ -0,0 +1,32 @@
+defmodule Strain do
+  @doc """
+  Given a `list` of items and a function `fun`, return the list of items where
+  `fun` returns true.
+
+  Do not use `Enum.filter`.
+  """
+  @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  def keep([], _fun), do: []
+  def keep([h|t], fun) do
+    cond do
+      fun.(h) == true -> [h] ++ keep(t, fun)
+      true -> keep(t, fun)
+    end
+  end
+
+  @doc """
+  Given a `list` of items and a function `fun`, return the list of items where
+  `fun` returns false.
+
+  Do not use `Enum.reject`.
+  """
+  @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  def discard([], _fun), do: []
+  def discard([h|t], fun) do
+    cond do
+      fun.(h) == false -> [h] ++ discard(t, fun)
+      true -> discard(t, fun)
+    end
+  end
+
+end
